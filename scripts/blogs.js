49b5fc545819277786a6db8a6d51aed429c1c63e
Andrew Nolan <35940923+acnolan@users.noreply.github.com>
2023-05-03 21:39:22 -0400

    Looking good!

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,4 +1,4 @@
-// The data, temporarily stored here, should be moved to .json file
+// For local development with a changed blogData.json, set the values on line 2
 let blogData = {};
 let queryFilter = "";
 
@@ -30,7 +30,7 @@ function generatePosts() {
 
             // Create the post title element
             let postTitle = document.createElement("h3");
-            postTitle.textContent = post.Title;
+            postTitle.innerHTML = queryFilter ? highlightMatchingSearchText(post.Title) : post.Title;
             postTitle.className = "blog-title-line";
             titleDiv.appendChild(postTitle);
 
@@ -42,7 +42,7 @@ function generatePosts() {
 
             // Create the post description element
             let postDescription = document.createElement("p");
-            postDescription.innerHTML = post.Description;
+            postDescription.innerHTML = queryFilter ? highlightMatchingSearchText(post.Description) : post.Description;
             postDescription.className = "blog-description";
             blogEntry.appendChild(postDescription);
 
@@ -60,6 +60,10 @@ function generatePosts() {
             blogList.appendChild(blogEntry);
         }
     });
+
+    if (!document.getElementById("blog-list").innerHTML) {
+        document.getElementById("blog-list").innerHTML = "<h3>No matching blogs :'^(</h3>";
+    }
 }
 
 
@@ -69,41 +73,64 @@ function checkFilters(post) {
     return post.Tags.some(tag => viableTags.includes(tag)) && applySearchFiltering(post);
 }
 
+
+// Check if a post is part of the search
 function applySearchFiltering(post) {
     // If no search term, don't worry about it
     if (!queryFilter) {
         return true;
     }
+    
+    return scoreSearchResult(post) > 0;
+}
 
-    // Some things to try
-    // 1. Match full text vs substring (probably can't do full text easily without stemming so maybe a ranking later)
-    // 2. Stemming
-    // 3. Tokenizing (check if any search token shows up more = higher weight) (break both text and query into tokens)
-    // 4. Synonyms
-    // 5. Add a sort by for relevant that is used when search is applied
-    // 6. Weighting in sort (maybe title is higher than description)
-    // 7. Highlight matching text
-    // 8. Fuzzy search?
-    post.Description.split(" ").forEach(textBlob => {
-        queryFilter.split(" ").forEach(query => {
-            return textBlob.toLowerCase().includes(query.toLowerCase());
+// Highlight text in title or description matched by search
+function highlightMatchingSearchText(textToMatch) {
+    queryFilter.split(" ").forEach(query => {
+        const indexOfMatch = textToMatch.toLowerCase().indexOf(query.toLowerCase());
+        if (indexOfMatch >= 0) {
+            textToMatch = textToMatch.substring(0, indexOfMatch) + '<em>' + textToMatch.substring(indexOfMatch, indexOfMatch + query.length) + '</em>' + textToMatch.substring(indexOfMatch + query.length);
+            textToMatch = textToMatch.replaceAll(query, '<em>' + query + '</em>');
+        }
+    });
+    
+    return textToMatch;
+}
+
+/**
+ * Score the post in terms of the search query
+ * Uses a sort of tokenizing method
+ * 
+ * Points:
+ * +1.5 matching title token
+ * +1 matching description token
+ * +0.5 partial match title
+ * +0.25 partial match description
+ * 
+ * @param {object} post 
+ */
+function scoreSearchResult(post) {
+    let searchWeight = 0;
+
+    queryFilter.split(" ").forEach(query => {
+        post.Title.split(" ").forEach(word => {
+            if (word.toLowerCase() === query.toLowerCase()) {
+                searchWeight += 1.5;
+            } else if (word.toLowerCase().includes(query.toLowerCase())) {
+                searchWeight += 0.75;
+            }
+        });
+
+        post.Description.split(" ").forEach(word => {
+            if (word.toLowerCase() === query.toLowerCase()) {
+                searchWeight += 1;
+            } else if (word.toLowerCase().includes(query.toLowerCase())) {
+                searchWeight += 0.5;
+            }
         });
     });
-    return post.Description.toLowerCase().includes(queryFilter.toLowerCase()) || post.Title.toLowerCase().includes(queryFilter.toLowerCase())
-}
 
-function highlightMatchingSearchText(textToMatch) {
-    // Break query into tokens and loop
-        // Break textToMatch into tokens and check if it matches
-            // add <em> tags around matches
-}
-
-function sortBySearch() {
-    // Weigh each post by the search sort criteria
-    // 1. +1.5 for matching tokens to query in title
-    // 2. +1 for matching tokens to query in description
-    // 3. +0.5 matching substring of token in title
-    // 4. +0.25 matching substring of token in description
+    return searchWeight;
 }
 
 
@@ -133,7 +160,10 @@ function getViableTags() {
 
 // Update the order of the document
 function updateSort() {
-    if (document.getElementById("sort-by").value === "0") {
+    if (queryFilter) {
+        document.getElementById('sort-by').innerHTML = "<option>Search Relevance</option>";
+        blogData.posts.sort((a, b) => (scoreSearchResult(a) < scoreSearchResult(b) ? 1 : -1));
+    } else if (document.getElementById("sort-by").value === "0") {
         blogData.posts.sort((a, b) => (new Date(a.Date) < new Date(b.Date)) ? 1 : -1);
     } else {
         blogData.posts.sort((a, b) => (new Date(a.Date) > new Date(b.Date)) ? 1 : -1);
@@ -141,16 +171,18 @@ function updateSort() {
     generatePosts();
 }
 
-/**
- * Search for a query term
- * 
- * @param {string} query 
- */
+// Search for something!
 function search(e) {
     e.preventDefault();
     const data = new FormData(e.target);
     queryFilter = Object.fromEntries(data.entries())?.query;
-    generatePosts();
+
+    if (!queryFilter) {
+        document.getElementById('sort-by').innerHTML = '<option value="0">Newest First</option><option value="1">Oldest First</option>';
+    }
+
+    updateSort();
   }
   
-  document.querySelector('#searchBar').addEventListener('submit', search);
+  // Add the listener to the search bar form
+  document.querySelector('#search-bar').addEventListener('submit', search);
