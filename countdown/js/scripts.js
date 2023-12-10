c41d621b334d94afefb90c53f9951a29d4a08a0f
Andrew Nolan <35940923+acnolan@users.noreply.github.com>
2023-12-09 19:06:10 -0500

    lint my files

--- countdown/js/scripts.js
+++ countdown/js/scripts.js
@@ -1,106 +1,106 @@
-/*Timer Stuff*/
+/* Timer Stuff */
 
-var countDownDate = new Date("Jun 24, 2021 18:00:00").getTime();
+const countDownDate = new Date('Jun 24, 2021 18:00:00').getTime();
 
-var x = setInterval(function(){
-    var now = new Date().getTime();
+const x = setInterval(() => {
+  const now = new Date().getTime();
 
-    var difference = countDownDate - now;
+  const difference = countDownDate - now;
 
-    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-    if(minutes < 10){
-        minutes = "0"+minutes;
-    }
-    if(seconds < 10){
-        seconds = "0"+seconds;
-    }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
 
-    document.getElementById("timearea").innerHTML = days + " days " + hours + " hours " + minutes + " minutes and " + seconds + " seconds!";
+  document.getElementById('timearea').innerHTML = `${days} days ${hours} hours ${minutes} minutes and ${seconds} seconds!`;
 
-    if (difference < 0){
-        clearInterval(x);
-        document.getElementById("timearea").innerHTML = "right now!";
-        // document.getElementById("timearea").innerHTML = "Someday, but even if we are far away, I love you everyday!";
-        document.getElementById("message").style.display = "none";
-    }
+  if (difference < 0) {
+    clearInterval(x);
+    document.getElementById('timearea').innerHTML = 'right now!';
+    document.getElementById('message').style.display = 'none';
+  }
 }, 1000);
 
-/*Heart Moving Stuff*/
-var heart1 = document.getElementById("heart1");
-var heart2 = document.getElementById("heart2");
-var heart3 = document.getElementById("heart3");
-document.addEventListener("mousemove",getMouse);
+let mouseX = 0;
+let mouseY = 0;
 
-heart1.style.position = "absolute";
-var heart1x = 0;
-var heart1y = 0;
+function getMouse(e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+}
 
-heart2.style.position = "absolute";
-var heart2x = 0;
-var heart2y = 0;
+/* Heart Moving Stuff */
+const heart1 = document.getElementById('heart1');
+const heart2 = document.getElementById('heart2');
+const heart3 = document.getElementById('heart3');
+document.addEventListener('mousemove', getMouse);
 
-heart3.style.position = "absolute";
-var heart3x = 0;
-var heart3y = 0;
+heart1.style.position = 'absolute';
+let heart1x = 0;
+let heart1y = 0;
+
+heart2.style.position = 'absolute';
+let heart2x = 0;
+let heart2y = 0;
+
+heart3.style.position = 'absolute';
+let heart3x = 0;
+let heart3y = 0;
+
+function followMouse() {
+  const distX1 = mouseX - heart1x;
+  const distY1 = mouseY - heart1y;
+
+  const distX2 = mouseX - heart2x;
+  const distY2 = mouseY - heart2y;
+
+  const distX3 = mouseX - heart3x;
+  const distY3 = mouseY - heart3y;
+
+  heart1x += distX1 / 5;
+  heart1y += distY1 / 5;
+
+  heart2x += distX2 / 3;
+  heart2y += distY2 / 3;
+
+  heart3x += distX3 / 1;
+  heart3y += distY3 / 1;
+
+  heart1.style.left = `${heart1x}px`;
+  heart1.style.top = `${heart1y}px`;
+
+  heart2.style.left = `${heart2x}px`;
+  heart2.style.top = `${heart2y}px`;
+
+  heart3.style.left = `${heart3x}px`;
+  heart3.style.top = `${heart3y}px`;
+}
 
 setInterval(followMouse, 50);
 
-var mouseX = 0;
-var mouseY = 0;
-
-function getMouse(e){
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-}
-
-function followMouse(){
-    var distX1 = mouseX - heart1x;
-    var distY1 = mouseY - heart1y;
-
-    var distX2 = mouseX - heart2x;
-    var distY2 = mouseY - heart2y;
-
-    var distX3 = mouseX - heart3x;
-    var distY3 = mouseY - heart3y;
-
-    heart1x += distX1/5;
-    heart1y += distY1/5;
-
-    heart2x += distX2/3;
-    heart2y += distY2/3;
-
-    heart3x += distX3/1;
-    heart3y += distY3/1;
-
-    heart1.style.left = heart1x + "px";
-    heart1.style.top = heart1y + "px";
-
-    heart2.style.left = heart2x + "px";
-    heart2.style.top = heart2y + "px";
-
-    heart3.style.left = heart3x + "px";
-    heart3.style.top = heart3y + "px";
-}
-
-/** Heart explosion **/
-function heartExplosion(){
-    for(i=0;i<15;i++){
-        let heart = document.createElement('img');
-        heart.src = "./images/heart.png";
-        heart.style.width = "32px";
-        heart.style.height = "32px";
-        heart.style.position = "absolute";
-        let randomX = Math.floor(Math.random() * 180 - 90)
-        let randomY = Math.floor(Math.random() * 180 - 90)
-        heart.style.left = mouseX + randomX + "px";
-        heart.style.top = mouseY + randomY +  "px";
-        heart.style.pointerEvents = "none";
-        let randomFade = (Math.random() * 3 + 1);
-        heart.style.animation = "fadeout "+randomFade+"s linear forwards";
-        document.body.appendChild(heart);
-    }
+/** Heart explosion * */
+// eslint-disable-next-line no-unused-vars
+function heartExplosion() {
+  for (let i = 0; i < 15; i += 1) {
+    const heart = document.createElement('img');
+    heart.src = './images/heart.png';
+    heart.style.width = '32px';
+    heart.style.height = '32px';
+    heart.style.position = 'absolute';
+    const randomX = Math.floor(Math.random() * 180 - 90);
+    const randomY = Math.floor(Math.random() * 180 - 90);
+    heart.style.left = `${mouseX + randomX}px`;
+    heart.style.top = `${mouseY + randomY}px`;
+    heart.style.pointerEvents = 'none';
+    const randomFade = (Math.random() * 3 + 1);
+    heart.style.animation = `fadeout ${randomFade}s linear forwards`;
+    document.body.appendChild(heart);
+  }
 }
