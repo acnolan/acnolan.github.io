c1fb470e7c633f46fe794691b17fead99cf73c78
Andrew Nolan <35940923+acnolan@users.noreply.github.com>
2025-06-04 12:17:03 -0400

    Website Redesign (#80)
    * New index
    
    * Update blog title
    
    * Cloud prototype and remove twitter
    
    * Working on big screens
    
    * CSS cleanup
    
    * Toggle dark mode everywhere
    
    * Update about me page
    
    * Handle resize, needs code clean up
    
    * More about me stuff
    
    * Update some dates and add comments

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -205,7 +205,7 @@ function generatePosts() {
       // Add a link to the blog
       // This is more semantic and better for accessibility than a JavaScript onclick
       const blogLink = document.createElement('a');
-      blogLink.href = `./blogs/${post.Key}`;
+      blogLink.href = `./blogs/${post.Key}.html`;
       blogLink.className = 'blog-link';
       blogLink.ariaLabel = post.Title;
       blogEntry.appendChild(blogLink);
@@ -253,7 +253,7 @@ function search(e) {
 // Add the listener to the search bar form
 document.querySelector('#search-bar').addEventListener('submit', search);
 
-// Get the blog
+// Get the blogs
 fetch('https://andrewnolan.dev/blogs/blogData.json')
   .then((res) => res.json())
   .then((json) => {
